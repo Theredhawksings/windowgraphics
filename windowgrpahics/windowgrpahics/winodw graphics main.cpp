c9bf72a3034a1a0507347f7ca84a2fd71028301f
Theredhawksings <myeoseul@gmail.com>
2024-09-25 10:26:17 +0900

    윈도우 그래픽 시작

---
+++ windowgrpahics/windowgrpahics/winodw graphics main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <GL/freeglut_ext.h>
+
+GLvoid drawScene() {
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+GLvoid Reshape(int w, int h) {
+    glViewport(0, 0, w, h);
+}
+
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(200, 200);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Example1");
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Unable to initialize GLEW" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    else {
+        std::cout << "GLEW Initialized\n";
+    }
+
+    glutDisplayFunc(drawScene);
+    glutReshapeFunc(Reshape);
+    glutMainLoop();
+}
+
