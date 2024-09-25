f9c42fcfcdeb63282f16e85e7f881077069567a2
Theredhawksings <myeoseul@gmail.com>
2024-09-25 20:36:08 +0900

    2번 4개 사각형 생성

--- gamegraphic/gamegraphic/windowgraphics.cpp
+++ gamegraphic/gamegraphic/windowgraphics.cpp
@@ -10,16 +10,32 @@ double B = 0;
 
 bool timer = false;
 
+#define X 800
+#define Y 600
+
 GLvoid drawScene() {
     glClearColor(R / 255.f, G / 255.f, B / 255.f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    //왼쪽 하단 오른쪽 상단
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glRectf(X/-0.2f, 0, 0, Y/0.2f);
+
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glRectf(0, 0, X/0.2f, Y/0.2f); 
+
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glRectf(X / -0.2f, Y / -0.2f, 0, 0);
+
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glRectf(0, Y/-0.2f, X/0.2f, 0);
+
     glutSwapBuffers();
 }
 
 GLvoid Reshape(int w, int h) {
     glViewport(0, 0, w, h);
 }
-
 
 
 int main(int argc, char** argv) {
